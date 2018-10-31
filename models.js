15f556e1552111af454cf5c12cbd68d66846cf55
ernest-phillips <ernestlphillips@gmail.com>
2018-10-30 21:16:23 -0700

    final commit

--- models.js
+++ models.js
@@ -3,16 +3,34 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const blogPostSchema = mongoose.Schema({
-    author: {
-        firstName: String,
-        lastName: String
-    },
-    title: { type: String, required: true },
-    content: { type: String },
-    created: { type: Date, default: Date.now }
+var authorSchema = mongoose.Schema({
+    firstName: 'string',
+    lastName: 'string',
+    userName: {
+        type: 'string',
+        unique: true
+    }
 });
 
+var commentSchema = mongoose.Schema({ contet: 'string' });
+
+// This went from const to var. Why?
+var blogPostSchema = mongoose.Schema({
+    title: 'string',
+    content: 'string',
+    created: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+    comments: [commentSchema]
+});
+
+blogPostSchema.pre('find', function(next) {
+    this.populate('author');
+    next();
+});
+
+blogPostSchema.pre('findOne', function(next) {
+    this.populate('author');
+    next();
+});
 
 blogPostSchema.virtual('authorName').get(function() {
     return `${this.author.firstName} ${this.author.lastName}`.trim();
@@ -24,10 +42,11 @@ blogPostSchema.methods.serialize = function() {
         author: this.authorName,
         content: this.content,
         title: this.title,
-        created: this.created
+        comments: this.comments
     };
 };
 
+var Author = mongoose.model('Author', authorSchema);
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = { BlogPosts };
+module.exports = { Author, BlogPosts };
--- server.js
+++ server.js
@@ -13,24 +13,156 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
+//GET Authors
+app.get('/authors', (req, res) => {
+    Author
+        .find()
+        .then(authors => {
+            res.json(authors.map(author => {
+                return {
+                    id: author._id,
+                    name: `${author.firstName} ${author.lastName}`,
+                    userName: author.userName
+                };
+            }));
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went wrong' });
+        });
+})
+
+app.post('/authors', (req, res) => {
+    const requiredFields = ['firstName', 'lastName', 'userName'];
+    requiredFields.forEach(field => {
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    });
+
+    Author
+        .findOne({ userName: req.body.userName })
+        .then(author => {
+            if (author) {
+                const message = `Username already taken`;
+                console.error(message);
+                return res.status(400).send(message);
+            } else {
+                Author
+                    .create({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        userName: req.body.userName
+                    })
+                    .then(author => res.status(201).json({
+                        _id: author.id,
+                        name: `${author.firstName} ${author.lastName}`,
+                        userName: author.userName
+                    }))
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({ error: 'Something went wrong' });
+                    });
+            }
+        })
+        .catch(err => {
+            ConstantSourceNode.error(err);
+            res.status(500).json({ error: 'something went horribly wrong' });
+        });
+});
+/////////////////////////////////////
+app.put('/authors/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
+    }
+    const updated = {};
+    const updatedableFields = ['firstName', 'lastName', 'userName'];
+    updatedableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+
+    Author
+        .findOne({ userName: updated.userName || '', _id: { $ne: req.params.id } })
+        .then(author => {
+            if (author) {
+                const message = `Username already taken`;
+                console.error(message);
+                return res.status(400).send(message);
+            } else {
+                Author
+                    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+                    .then(updatedAuthor => {
+                        res.status(200).json({
+                            id: updatedAuthor.id,
+                            name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+                            userName: updatedAuthor.userName
+                        });
+                    })
+                    .catch(err => res.status(500).json({ message: err }));
+            }
+        });
+});
+
+app.delete('/authors/:id', (req, res) => {
+    BlogPost
+        .remove({ author: req.params.id })
+        .then(() => {
+            Author
+                .findByIdAndRemove(req.params.id)
+                .then(() => {
+                    console.log(`Deleted blog posts owned by an author with id \`${req.params.id}\``);
+                    res.status(204).json({ message: 'success' });
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went wrong' });
+        });
+});
+
+
 app.get('/posts', (req, res) => {
     BlogPost
-        .findOne(req.title)
-        .populate('author')
+        .find()
         .then(posts => {
-            res.json(posts.map(post => post.serialize()));
+            res.json(posts.map(post => {
+                return {
+                    id: post._id,
+                    author: post.authorName,
+                    content: post.content,
+                    title: post.title
+                };
+            }));
         })
         .catch(err => {
             console.error(err);
             res.status(500).json({ error: 'Something went terribly wrong' });
         });
 });
+
+
 //READ
 app.get('/posts/:id', (req, res) => {
     BlogPost
 
-        .findById(req.params.id)
-        .then(post => res.json(post.serialze()))
+        .find()
+        .then(post => {
+            res.json(posts.map(post => {
+                return {
+                    id: post._id,
+                    author: post.authorName,
+                    content: post.content,
+                    title: post.title
+                };
+            }));
+        })
         .catch(err => {
             console.error(err);
             res.status(500).json({ error: 'Something went very bad.' });
@@ -40,8 +172,7 @@ app.get('/posts/:id', (req, res) => {
 //CREATE
 app.post('/posts', (req, res) => {
     const requiredFields = ['title', 'content', 'author'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i]; //iterate through list and assign to new list
+    requiredFields.forEach(field => {
         if (!(field in req.body)) {
             const message = `Missing \`${field}\` in request body`;
             console.error(message); //log error message to console
@@ -49,37 +180,42 @@ app.post('/posts', (req, res) => {
             //due to malformed syntax. 
             return res.status(400).send(message);
         }
-    }
+    })
 
-    BlogPost
-        .create({
-            title: req.body.title,
-            content: req.body.content,
-            author: req.body.author
-        })
-        //The request has been fulfilled and resulted in a new resource being created. 
-        .then(blogPost => res.status(201).json(blogPost.serialize()))
-        .catch(err => {
-            res.status(500).json({ error: 'Something went wrong' });
-        });
-
-});
-
-//DELETE
-app.delete('/posts/:id', (req, res) => {
-    BlogPost
-        .findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).json({ message: 'success' });
-            //The server has fulfilled the request but does not need to return an entity-body
+    Author
+        .findById(req.body.author_id)
+        .then(author => {
+            if (author) {
+                BlogPost
+                    .create({
+                        title: req.body.title,
+                        content: req.body.content,
+                        author: req.body.id
+                    })
+                    .then(blogPost => res.status(201).json({
+                        id: blogPost.id,
+                        author: `${author.firstName} ${author.lastName}`,
+                        content: blogPost.content,
+                        title: blogPost.title,
+                        comments: blogPost.comments
+                    }))
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({ error: 'Something went wrong' });
+                    });
+            } else {
+                const message = `Author not found`;
+                console.error(message);
+                return res.status(400).send(message);
+            }
         })
         .catch(err => {
             console.error(err);
-            res.status(500).json({ error: 'something went terribly wrong' });
+            res.status(500).json({ error: 'something went horribly awry' });
         });
-});
 
-//UPDATE
+});
+//***********************
 app.put('/posts/:id', (req, res) => {
     if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
         res.status(400).json({
@@ -97,12 +233,16 @@ app.put('/posts/:id', (req, res) => {
     BlogPost //////what is the $set here?
     //https://docs.mongodb.com/manual/reference/operator/update/set/
         .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-        .then(updatedPost => res.status(204))
+        .then(updatedPost => res.status(200).json({
+            id: updatedPost.id,
+            title: updatedPost.title,
+            content: updatedPost.content
+        }))
         .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
 //DELETE
-app.delete('/:id', (req, res) => {
+app.delete('/posts/:id', (req, res) => {
     BlogPost
         .findByIdAndRemove(req.params.id)
         .then(() => {
@@ -111,6 +251,7 @@ app.delete('/:id', (req, res) => {
         });
 });
 // Why couldn't arrow function be used here?
+//What does the asterisk do here?
 app.use('*', function(req, res) {
     res.status(404).json({ message: 'Not Found' });
 });
@@ -156,3 +297,21 @@ if (require.main === module) {
 }
 
 module.exports = { runServer, app, closeServer };
+
+
+
+//DELETE Deprecated code
+// app.delete('/posts/:id', (req, res) => {
+//     BlogPost
+//         .findByIdAndRemove(req.params.id)
+//         .then(() => {
+//             res.status(204).json({ message: 'success' });
+//             //The server has fulfilled the request but does not need to return an entity-body
+//         })
+//         .catch(err => {
+//             console.error(err);
+//             res.status(500).json({ error: 'something went terribly wrong' });
+//         });
+// });
+
+//UPDATE
